e6a317ebc85679205ccac0ab478d6f1b3ac48b78
bugrasarilan <bugrasarilan@gmail.com>
2022-11-11 16:19:53 +0300

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+import "./form.css"
+
+function Form() {
+    const [forms, setForms] = useState("");
+    const [todos, setTodos] = useState([]);
+    //console.log(todos)
+    const handlesubmit = (e) => {
+        e.preventDefault();
+    
+       setTodos([{forms}, ...todos]) 
+       setForms("");
+    }
+
+    return (
+        <div className=' form'>
+            <form onSubmit={handlesubmit}>
+                <input className='input'
+                    placeholder='todo'
+                    type="text"
+                    onChange={(e) => setForms(e.target.value)}
+                    value={forms}
+                    
+                />
+
+                <button className='button' type='submit'> add</button>
+
+            </form>
+            <ul>
+                {
+                    todos.map((t) => (
+                        <li className='list'>
+                            <span> {t.forms} </span>
+                            <button> edit</button>
+                            <button> delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+
+    )
+}
+
+export default Form
